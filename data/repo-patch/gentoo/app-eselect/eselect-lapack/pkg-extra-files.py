413c44173f93dac8d74138afc83604540ebd4705
fpemud <fpemud@sina.com>
2021-01-21 09:16:56 +0800

    fix

--- data/repo-patch/gentoo/app-eselect/eselect-lapack/pkg-extra-files.py
+++ data/repo-patch/gentoo/app-eselect/eselect-lapack/pkg-extra-files.py
@@ -9,10 +9,10 @@ for fn in glob.glob("*.ebuild"):
         f.write("""
 pkg_extra_files()
 {
-	echo "/usr/lib64/liblapack.a"
-	echo "/usr/lib64/liblapack.so"
-	echo "/usr/lib64/liblapack.so.3"
-	echo "/usr/lib64/pkgconfig/lapack.pc"
+        echo "/usr/lib64/liblapack.a"
+        echo "/usr/lib64/liblapack.so"
+        echo "/usr/lib64/liblapack.so.3"
+        echo "/usr/lib64/pkgconfig/lapack.pc"
 }
 """)
     subprocess.run(["ebuild", fn, "manifest"], stdout=subprocess.DEVNULL)
